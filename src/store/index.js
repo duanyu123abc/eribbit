492404621487869dfbc47ba5d7e523f5b8f56c4f
duanyu <1136177119@qq.com>
2022-05-16 22:01:50 +0800

    小兔仙初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.Store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
   state: {
+    // 存状态
   },
   getters: {
+    // 写计算属性
   },
   mutations: {
+    // 修改方法
   },
   actions: {
+    // 拿数据
   },
   modules: {
+    // 分模块
   }
 })
