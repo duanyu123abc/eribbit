82fb60868a2cfce5aeacbe8fcd069bf7ddab4ae9
duanyu <1136177119@qq.com>
2022-05-18 21:48:19 +0800

    请求工具-请求函数

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 // 根组件
 <template>
 <!-- vue2.0需要根元素 vue3.0可以是代码片段 Fragment -->
-<div>
-  App
+<div class="container">
+  App{{$store.state.user.profile.account}}
+  <button @click="$store.commit('user/setUser', {account: 'dy'})">设置用户信息</button>
 </div>
-<p>p</p>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+
+import cart from './modules/cart'
+import category from './modules/category'
+import user from './modules/user'
 
 // vue2.0 创建仓库 new Vuex.Store({})
 // vue3.0 创建仓库 createStore({})
@@ -17,5 +22,17 @@ export default createStore({
   },
   modules: {
     // 分模块
-  }
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    // 默认存储在localStorage
+    createPersistedstate({
+      // 本地存储名字
+      key: 'erabbit-client-pc-124-store',
+      // 指定需要存储的模块
+      path: ['user', 'cart']
+    })
+  ]
 })
