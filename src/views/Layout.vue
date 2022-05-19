3b683d4450eead742f3ef931685d5dee7213a5f7
duanyu <1136177119@qq.com>
2022-05-19 17:23:44 +0800

    头部底部组件

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,32 @@
 <template>
-  <nav>顶部通栏</nav>
-  <header>头部</header>
-  <main>
+<!-- 顶部通栏 -->
+  <AppNavBar></AppNavBar>
+  <!-- 头部组件 -->
+  <AppHeader></AppHeader>
+  <div class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
-  </main>
-  <footer>底部</footer>
+  </div>
+  <!-- 底部组件 -->
+  <AppFooter></AppFooter>
 </template>
 
 <script>
+import AppNavBar from '@/components/app-navbar.vue'
+import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    AppNavBar,
+    AppHeader,
+    AppFooter
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.app-body {
+  min-height: 600px;
+}
+</style>
