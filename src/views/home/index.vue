36917cc6d398bd68ff11ef7f350fbeddec071b0a
duanyu <1136177119@qq.com>
2022-05-25 16:59:14 +0800

    轮播图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class='container'>
     <!-- 首页入口 -->
     <div class="home-entry">
+      <div class='container'>
       <!-- 左侧分类 -->
       <HomeCategory></HomeCategory>
       <!-- 轮播图 -->
+      <HomeBanner></HomeBanner>
     </div>
-
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from '@/views/home/components/home-category.vue'
+import HomeBanner from '@/views/home/components/home-banner.vue'
 export default {
   name: 'HomePage',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
